18f2924f42fc87e1e5191cf679c94e5267a034d0
realkelvinworld <4231210031@live.gctu.edu.gh>
2025-11-20 13:43:57 +0000

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
           width: "100%",
           height: "100vh",
           position: "relative",
+          backgroundColor: "#000",
         }}
       >
         <Beams
