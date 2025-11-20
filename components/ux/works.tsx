d86812b72cbbafa58c07699348c3418c4c3fe838
realkelvinworld <4231210031@live.gctu.edu.gh>
2025-11-20 10:20:45 +0000

    updates

--- components/ux/works.tsx
+++ components/ux/works.tsx
@@ -90,7 +90,7 @@ export function WorkCards() {
                     style={{
                       borderRadius: "12px",
                     }}
-                    className="relative h-[400px] lg:w-[350px] w-85 border bg-white"
+                    className="relative h-[500px] lg:w-[350px] w-80 border bg-white"
                   >
                     <ScrollArea className="h-[90vh]" type="scroll">
                       <div className="relative p-6">
